39c0a1b1935d20cfaf42c38f0d44e5e9530c3a3f
Saiteja Dhondi <saitejadhondi34111@gmail.com>
2025-08-22 23:13:36 +0530

    Added New Files

--- 01 Basic/02 Patterns/Pattern 11.cpp
+++ 01 Basic/02 Patterns/Pattern 11.cpp
@@ -1,3 +1,13 @@
+/*
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+1 
+0 1 
+1 0 1 
+0 1 0 1 
+1 0 1 0 1
+
+*/
 #include<bits/stdc++.h>
 using namespace std;
 class Solution
@@ -5,11 +15,18 @@ class Solution
     public:
     void PrintStars(int n)
     {
-        for(int i=1; i<=n; i++)
+        int start=1;
+        for(int i=1;i<=n;i++)
         {
-            for(int j=1; j<=n; j++)
+            if(i%2==0)
             {
-                cout << "*";
+                start=0;
+            }
+            else start=1;
+            for(int j=1;j<=i;j++)
+            {
+                cout <<start <<" ";
+                start=1-start;
             }
             cout << endl;
         }
