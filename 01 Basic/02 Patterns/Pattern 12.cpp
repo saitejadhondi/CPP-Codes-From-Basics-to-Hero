3a9423a8b1ca1af61d466c825fe1956c353368ac
Saiteja Dhondi <saitejadhondi34111@gmail.com>
2025-08-23 17:48:30 +0530

    Added New Files

--- 01 Basic/02 Patterns/Pattern 12.cpp
+++ 01 Basic/02 Patterns/Pattern 12.cpp
@@ -1,3 +1,13 @@
+/*
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+1        1
+12      21
+123    321
+1234  4321
+1234554321
+
+*/
 #include<bits/stdc++.h>
 using namespace std;
 class Solution
@@ -5,12 +15,22 @@ class Solution
     public:
     void PrintStars(int n)
     {
-        for(int i=1; i<=n; i++)
+        int spaces=n-1;
+        for(int i=1;i<=n;i++)
         {
-            for(int j=1; j<=n; j++)
+            for(int j=1;j<=i;j++)
             {
-                cout << "*";
+                cout <<j;
             }
+            for(int j=1;j<=2*spaces;j++)
+            {
+                cout <<" ";
+            }
+            for(int j=i;j>=1;j--)
+            {
+                cout <<j;
+            }
+            spaces=spaces-1;
             cout << endl;
         }
     }
--- 01 Basic/02 Patterns/Pattern 13.cpp
+++ 01 Basic/02 Patterns/Pattern 13.cpp
@@ -1,3 +1,13 @@
+/*
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+1 
+2 3 
+4 5 6 
+7 8 9 10 
+11 12 13 14 15
+
+*/
 #include<bits/stdc++.h>
 using namespace std;
 class Solution
@@ -5,11 +15,13 @@ class Solution
     public:
     void PrintStars(int n)
     {
+        int num=1;
         for(int i=1; i<=n; i++)
         {
-            for(int j=1; j<=n; j++)
+            for(int j=1; j<=i; j++)
             {
-                cout << "*";
+                cout << num <<" ";
+                num++;
             }
             cout << endl;
         }
