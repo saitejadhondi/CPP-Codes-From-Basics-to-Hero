b18779176d70202ebec1db1e36d25f97202d711b
Saiteja Dhondi <saitejadhondi34111@gmail.com>
2025-08-23 19:11:14 +0530

    Added New Files

--- 01 Basic/02 Patterns/Pattern 14.cpp
+++ 01 Basic/02 Patterns/Pattern 14.cpp
@@ -1,3 +1,13 @@
+/*
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+A
+AB
+ABC
+ABCD
+ABCDE
+
+*/
 #include<bits/stdc++.h>
 using namespace std;
 class Solution
@@ -5,14 +15,14 @@ class Solution
     public:
     void PrintStars(int n)
     {
-        for(int i=1; i<=n; i++)
+        for(int i=0; i<n; i++)
         {
-            for(int j=1; j<=n; j++)
+            for(char j='A'; j<='A'+i; j++)
             {
-                cout << "*";
+                cout << j;
             }
             cout << endl;
-        }
+        } 
     }
 };
 int main()
--- 01 Basic/02 Patterns/Pattern 15.cpp
+++ 01 Basic/02 Patterns/Pattern 15.cpp
@@ -5,11 +5,11 @@ class Solution
     public:
     void PrintStars(int n)
     {
-        for(int i=1; i<=n; i++)
+        for(int i=n-1; i>=0; i--)
         {
-            for(int j=1; j<=n; j++)
+            for(char j='A'; j<='A'+i; j++)
             {
-                cout << "*";
+                cout << j;
             }
             cout << endl;
         }
