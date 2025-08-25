c23638b9ba9446825085349fc2dd0fbf26657421
Saiteja Dhondi <saitejadhondi34111@gmail.com>
2025-08-25 11:59:35 +0530

    New Files Added

--- 01 Basic/02 Patterns/Pattern 20.cpp
+++ 01 Basic/02 Patterns/Pattern 20.cpp
@@ -1,3 +1,17 @@
+/*
+Given an integer n. You need to recreate the pattern given below for any value of N. Let's say for N = 5, the pattern should look like as below:
+
+*        *
+**      **
+***    ***
+****  ****
+**********
+****  ****
+***    ***
+**      **
+*        *
+
+*/
 #include<bits/stdc++.h>
 using namespace std;
 class Solution
@@ -5,12 +19,40 @@ class Solution
     public:
     void PrintStars(int n)
     {
+        int spaces=2*n-2;
         for(int i=1; i<=n; i++)
         {
-            for(int j=1; j<=n; j++)
+            for(int j=1; j<=i; j++)
             {
                 cout << "*";
             }
+            for(int j=1; j<=spaces; j++)
+            {
+                cout << " ";
+            }
+            for(int j=1; j<=i; j++)
+            {
+                cout << "*";
+            }
+            spaces=spaces-2;
+            cout << endl;
+        }
+        spaces=2;
+        for(int i=n-1; i>=1; i--)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                cout << "*";
+            }
+            for(int j=1; j<=spaces; j++)
+            {
+                cout << " ";
+            }
+            for(int j=1; j<=i; j++)
+            {
+                cout << "*";
+            }
+            spaces=spaces+2;
             cout << endl;
         }
     }
