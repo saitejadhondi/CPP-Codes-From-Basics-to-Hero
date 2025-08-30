acbb69d24b81a5b5e1bcc81464dedc1cb846c56b
Saiteja Dhondi <saitejadhondi34111@gmail.com>
2025-08-30 19:48:25 +0530

    Added New Files

--- 01 Basic/03 Basic Maths/10 Count of Prime Numbers till N.cpp
+++ 01 Basic/03 Basic Maths/10 Count of Prime Numbers till N.cpp
@@ -1,26 +1,49 @@
 /*
 
 You are given an integer n. 
-Return the integer formed by 
-placing the digits of n in reverse order.
+You need to find out the number of prime numbers 
+in the range [1, n] (inclusive). 
+Return the number of prime numbers in the range.
+A prime number is a number which has no divisors except, 1 and itself.
+
+Complexity Analysis:
+Time Complexity: O(N2) – Checking all numbers from 1 to n for prime and checking if a number is prime or not will take O(n) TC.
+
+Space Complexity: O(1) – Using a couple of variables i.e., constant space.
 
 */
+
+//Method 1
 
 #include<bits/stdc++.h>
 using namespace std;
 
 class Solution{
-    public:
-        int reverseNumber(int n) {
-        int lastDigit;
-        int revNum=0;
-        while(n>0)
+    private:
+    bool isPrime(int n)
+    {
+        if(n==2) return true;
+        for(int i=2;i<n;i++)
         {
-            lastDigit = n%10;
-            revNum = revNum*10+lastDigit;
-            n=n/10;
+            if(n%i==0)
+            {
+                return false;
+            }
         }
-        return revNum;
+        return true; 
+    }
+public:
+    int primeUptoN(int n) {
+        if(n<2)
+        {
+            return 0;
+        }
+        int count=0;
+        for(int i=2;i<=n;i++)
+        {
+            if(isPrime(i)) count++;
+        }
+        return count;
     }
 };
 
@@ -29,6 +52,49 @@ int main()
     Solution sol;
     int n;
     cin >> n;
-    cout << sol.reverseNumber(n);
+    cout << sol.primeUptoN(n);
     return 0;
 }
+
+/*
+
+Method 2
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+private:
+    bool isPrime(int n) {
+        int count = 0;
+        for (int i = 1; i <= n; ++i) {
+            if (n % i == 0) {
+                count = count + 1;
+            }
+        }
+        if (count == 2) return true;
+        return false;
+    }
+    
+public:
+    int primeUptoN(int n) {
+        int count = 0;
+        for (int i = 2; i <= n; i++) {
+            
+            if (isPrime(i)) count++;
+        }
+        return count;
+    }
+};
+
+int main() {
+    int n = 6;
+    Solution sol; 
+    int ans = sol.primeUptoN(n);
+    cout << "The count of primes till " << n << " is: " << ans;
+    
+    return 0;
+}
+
+
+*/
