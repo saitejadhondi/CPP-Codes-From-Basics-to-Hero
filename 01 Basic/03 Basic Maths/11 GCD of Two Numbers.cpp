66f3d9fc8477ea1ebb05f53e628f89af8ff801af
Saiteja Dhondi <saitejadhondi34111@gmail.com>
2025-08-31 10:56:54 +0530

    Added New Files

--- 01 Basic/03 Basic Maths/11 GCD of Two Numbers.cpp
+++ 01 Basic/03 Basic Maths/11 GCD of Two Numbers.cpp
@@ -1,8 +1,12 @@
 /*
 
-You are given an integer n. 
-Return the integer formed by 
-placing the digits of n in reverse order.
+You are given two integers n1 and n2. You need find the Greatest Common Divisor (GCD) of the two given numbers. 
+Return the GCD of the two numbers.
+The Greatest Common Divisor (GCD) of two integers is the largest positive integer that divides both of the integers.
+
+Complexity Analysis:
+Time Complexity: O(min(N1, N2)) – where N1 and N2 are given numbers. Iterating from 1 to min(N1, N2) and performing constant time operations in each iteration.
+Space Complexity: O(1) – Using a couple of variables i.e., constant space.
 
 */
 
@@ -11,24 +15,57 @@ using namespace std;
 
 class Solution{
     public:
-        int reverseNumber(int n) {
-        int lastDigit;
-        int revNum=0;
-        while(n>0)
+        int GCD(int n1, int n2) {
+        int gcd;
+            if(n1<n1)
         {
-            lastDigit = n%10;
-            revNum = revNum*10+lastDigit;
-            n=n/10;
+            gcd=n1;
         }
-        return revNum;
+        else
+        {
+            gcd=n2;
+        } 
+        for(int i=gcd;i>=1;i--)
+        {
+            if((n1%i==0) && (n2%i==0))
+            {
+                return i;
+            }
+        }
+/*
+METHOD 2
+while(n1!=0)
+        {
+            if(n1<n2)
+            {
+                n2=n2-n1;
+            }
+            else n1=n1-n2;
+        }
+        return n2;
+*/
     }
 };
 
 int main()
 {
     Solution sol;
-    int n;
-    cin >> n;
-    cout << sol.reverseNumber(n);
+    int n1, n2;
+    cin >> n1;
+    cin >> n2;
+    cout << sol.GCD(n1, n2);
     return 0;
 }
+
+/*
+METHOD 2
+while(n1!=0)
+        {
+            if(n1<n2)
+            {
+                n2=n2-n1;
+            }
+            else n1=n1-n2;
+        }
+        return n2;
+*/
