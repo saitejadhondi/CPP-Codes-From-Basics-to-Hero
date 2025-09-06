30b724ea7ed2bde0c817b9768895ff3475e93ae7
Saiteja Dhondi <saitejadhondi34111@gmail.com>
2025-09-06 20:42:18 +0530

    Added New Files

---
+++ 01 Basic/05 Basic Hashing/01 Highest Occurring Element in an Array.cpp
@@ -0,0 +1,11 @@
+/*
+Given an array nums of n integers, 
+find the most frequent element in it i.e., the element that occurs the maximum number of times. 
+If there are multiple elements that appear a maximum number of times, find the smallest of them.
+*/
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+return 0;
+}
